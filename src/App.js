3f61ec9d720cbba3dae9e70167c96c9ba0e44498
AustinBeal <austinbeal89@gmail.com>
2021-03-06 14:48:52 -0600

    finished logic behind removing the items from ol using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  constructor (props){
+    super(props)
+
+    this.state = {
+      inputValue: "",
+      listOfTodos: []
+    }
+  }
+  
+
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.setState({listOfTodos: [...this.state.listOfTodos, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+
+  handleRemove = (index) => {
+    // this.state.listOfTodos.splice(index, 1)
+    console.log(index.target.value)
+    let list = this.state.listOfTodos
+    let splicey = index.target.value
+    list.splice(splicey, 1)
+    this.setState({listOfTodos: [...list]})
+  }
+
+  render(){
+
+      return (
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+            <button type="submit">GO</button>
+          </form>
+
+             <ul>{this.state.listOfTodos.map((todo, index) => {
+              //  console.log(todo, index)
+              return <div key={index} value={index} className='lidiv' >
+                <li> {todo} </li>
+                <button value={index} onClick={(index) => {this.handleRemove(index)}}>Done</button>
+              </div>
+              })}</ul>  
+
+          <div>
+            {this.props.name}
+          </div>
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/TodoCard.js
@@ -0,0 +1,21 @@
+// import React, { Component } from 'react';
+// import './App.css';
+
+
+
+// class TodoCard extends Component {
+//   constructor (props){
+//     super(props)
+//  let readytomap = 
+//   <ul>{this.state.listOfTodos.map((todo, index) => {
+//     return <div>
+//       <li key={index} remove={this.handleRemove}> {todo} </li>
+//       <button  onClick={this.handleRemove}>Done</button>
+//     </div>
+
+//   }</ul>}}
+  
+// }
+
+
+//  export default TodoCard;
